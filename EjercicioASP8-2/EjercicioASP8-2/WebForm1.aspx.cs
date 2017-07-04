71abed2d2e49bef890dfcd78f126a81a0d8e9d42
Hachiko92 <eleonora.franchini@tajamar365.com>
2017-07-04 17:10:03 +0200

    ejercicio

---
+++ EjercicioASP8-2/EjercicioASP8-2/WebForm1.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EjercicioASP8_2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable("Provincias");
+            dt.Columns.Add("ProvinciaID", typeof(int));
+            dt.Columns.Add("Nombre", typeof(string));
+
+            DataRow row1 = dt.NewRow();
+            row1["ProvinciaID"] = 8;
+            row1["Nombre"] = "Barcelona";
+            dt.Rows.Add(row1);
+
+            DataRow row2 = dt.NewRow();
+            row2["ProvinciaID"] = 28;
+            row2["Nombre"] = "Madrid";
+            dt.Rows.Add(row2);
+
+            DataRow[] currentRows = dt.Select(null, null, DataViewRowState.CurrentRows);
+            
+            if(currentRows.Length > 1)
+            {
+                foreach(DataColumn columna in dt.Columns)
+                {
+                    Response.Write("<strong>" + columna.ColumnName + " </strong>");
+                }
+
+                Response.Write("<br>");
+
+                foreach (DataRow fila in currentRows)
+                {
+                    foreach(DataColumn columuns in dt.Columns)
+                    {
+                        Response.Write(fila[columuns] + " ");
+                    }
+                    Response.Write("<br>");
+                }
+            }
+
+            Response.Write("<br><br><strong>Con DataView</strong><br>");
+
+            DataView provincias = new DataView(dt, null, null, DataViewRowState.CurrentRows);
+            foreach (DataRowView filas in provincias)
+            {
+                DataRow filaData = filas.Row;
+                foreach (DataColumn columnaData in dt.Columns)
+                {
+                    Response.Write(filaData[columnaData] + " ");
+                }
+                Response.Write("<br>");
+            }
+        }
+    }
+}
--- EjercicioASP8-3/EjercicioASP8-3/WebForm1.aspx.cs
+++ EjercicioASP8-3/EjercicioASP8-3/WebForm1.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +14,37 @@ namespace EjercicioASP8_3
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            string cadenaConexion = ConfigurationManager.ConnectionStrings["NorthWind"].ConnectionString;
+            string query = "SELECT ProductName, UnitsInStock FROM Products";
 
+            try
+            {
+                using(SqlConnection connection = new SqlConnection(cadenaConexion))
+                {
+                    connection.Open();
+
+                    SqlDataAdapter adaptador = new SqlDataAdapter(query, connection);
+                    DataSet productosDS = new DataSet();
+                    /* copia los resultados de la query en una copia virtual */
+                    adaptador.Fill(productosDS, "Products");
+
+                    DataView productosDV = new DataView(productosDS.Tables[0], "UnitsInStock > 50", "ProductName", DataViewRowState.CurrentRows);
+
+                    Response.Write("<strong>Productos con mas de 50 unidades in stock:</strong><br><br><ol>");
+                    foreach (DataRowView lineaDV in productosDV)
+                    {
+                        DataRow fila = lineaDV.Row;
+                        Response.Write("<li>" + lineaDV[0].ToString() + "<strong> " + lineaDV[1].ToString() + "</strong></li>");
+                    }
+                    Response.Write("</ol>");
+
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
         }
     }
 }
